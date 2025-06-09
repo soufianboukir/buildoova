3e28a11c1a3b86431534156c4d47744538338001
sofyan <soufianeboukir0@gmail.com>
2025-06-10 00:42:36 +0100

    publish site backend

---
+++ app/api/publish/route.ts
@@ -0,0 +1,47 @@
+import { auth } from "@/auth";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest): Promise<NextResponse> =>{
+    try{
+        const session = await auth()
+        if(!session){
+            return NextResponse.json({
+                message: 'Unauthorized'
+            },{
+                status: 401
+            })
+        }
+
+        const { code, siteName } = await request.json()
+        const siteNameExists = await User.findOne({siteName: siteName})
+        if(siteNameExists){
+            return NextResponse.json({
+                'message': 'Site name is not available'
+            },{
+                status: 404
+            })
+        }
+        const user = await User.findById(session.user.id);
+        if(!user){
+            return NextResponse.json({
+                'message': 'User not found'
+            },{
+                status: 404
+            })
+        }
+        user.siteName = siteName;
+        user.code = code;
+        await user.save();
+
+        return NextResponse.json({
+            'message': 'Your site has been published'
+        })
+    }catch{
+        return NextResponse.json({
+            message: 'An error occured from server'
+        },{
+            status: 500
+        })
+    }
+}
